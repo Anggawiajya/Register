84c3a3a9657afca1cedf8cad20df5b288da921e6
Anggawiajya <anggawija1233@gmail.com>
2024-08-13 09:45:50 +0700

    register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-// Database connection
+// Koneksi ke database
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -11,22 +11,22 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
+// Ambil data dari form
 $ucp = $_POST['ucp'];
 $verify_code = $_POST['verifycode'];
 
-// Check UCP in database
+// Cek UCP di database
 $ucp_query = "SELECT * FROM playerucp WHERE ucp = '$ucp'";
 $ucp_result = $conn->query($ucp_query);
 
 if ($ucp_result->num_rows > 0) {
-    // Check Verification Code in database
+    // Cek Verification Code di database
     $code_query = "SELECT * FROM playerucp WHERE verifycode = '$verify_code'";
     $code_result = $conn->query($code_query);
 
     if ($code_result->num_rows > 0) {
         echo "Registration successful!";
-        // Further processing can be added here like saving session or redirecting user
+        // Proses lebih lanjut bisa ditambahkan di sini, seperti menyimpan session atau mengarahkan user
     } else {
         echo "Verification code not found!";
     }
